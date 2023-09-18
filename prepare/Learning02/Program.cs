b81009edd19d3adfd64338c259eb905268ce326e
Jashua <77981430+81Jashua@users.noreply.github.com>
2023-09-18 13:14:07 -0600

    W02 Prep complete

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,12 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job newJob = new Job();
-        newJob._company = "Microsoft";
-        newJob._jobTitle = "Developer";
-        newJob._startYear = 2013;
-        newJob._endYear = 2021;
+        Job firstJob = new Job();
+        firstJob._company = "Microsoft";
+        firstJob._jobTitle = "Developer";
+        firstJob._startYear = 2013;
+        firstJob._endYear = 2021;
+
+        Job secondJob = new Job();
+        secondJob._company = "Apple";
+        secondJob._jobTitle = "Business Analyst";
+        secondJob._startYear = 2019;
+        secondJob._endYear = 2023;
         //Console.WriteLine($"{newJob._company} {newJob._jobTitle}");
-        newJob.Display();
+        //firstJob.Display();
+
+        Resume firstResume = new Resume();
+        firstResume._name = "Josh Snyder";
+        firstResume._jobs.Add(firstJob);
+        firstResume._jobs.Add(secondJob);
+
+        firstResume.Display();
     }
 }
