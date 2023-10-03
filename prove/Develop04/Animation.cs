2e0ac11ababb56183890b35a54140bd7cb27b3ac
Jashua <77981430+81Jashua@users.noreply.github.com>
2023-10-03 11:41:47 -0600

    Prove 04 added animation class

---
+++ prove/Develop04/Animation.cs
@@ -0,0 +1,27 @@
+public class Animation
+{
+    public static void RunAnimation(int seconds)
+    {
+        while (seconds != 0)
+        {
+            Console.Write("/");
+            Thread.Sleep(250);
+            Console.Write("\b \b"); // Erase the + character            
+            
+            Console.Write("-"); // Replace it with the - character
+            Thread.Sleep(250);
+            Console.Write("\b \b");            
+            
+            Console.Write("\\"); // Replace it with the - character
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+            
+            Console.Write("|"); // Replace it with the - character
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+            
+        
+            seconds -= 1;
+        }        
+    }
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+        //RunAnimation(10);
+        Animation.RunAnimation(10);        
     }
 }
